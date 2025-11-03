0c8d5141293c422869c9ea4f843dc5ac11444746
stmicroel <undefinedstepan@gmail.com>
2025-11-03 18:01:19 +0100

    Initial commit

---
+++ DualSensorTermometer/main.c
@@ -0,0 +1,17 @@
+/*
+ * DualSensorThermometer.c
+ *
+ * Created: 03.11.2025 0:27:24
+ * Author : stmicroel-avr
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void) {
+    while (1) {
+		
+    }
+}
+
